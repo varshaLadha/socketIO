dd103de4f68212cbb6b51294dfe577146fe43dba
varsha <lanetteam.varsha@gmail.com>
2018-02-28 22:23:23 +0530

    changes made

--- socketIO/server/server.js
+++ socketIO/server/server.js
@@ -1,12 +1,26 @@
-const path=require('path')
+const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const publicPath=path.join(__dirname,"../public")
 const port=process.env.PORT || 3000;
 var app=express();
 
+var server = http.createServer(app)
+var io = socketIO(server)
+
 app.use(express.static(publicPath))
 
-app.listen(3000,()=>{
+io.on('connection', (socket)=>{
+    console.log("new user connected")
+
+    socket.on('disconnect',() => {
+        console.log("user disconnected")
+    })
+})
+
+
+server.listen(3000,()=>{
     console.log("Connected to server on "+port)
 })
