9487dfc1da34565017293c462d016f63ce95a3fc
varsha <lanetteam.varsha@gmail.com>
2018-03-02 09:07:39 +0530

    creating user and notifying others

--- server/server.js
+++ server/server.js
@@ -15,28 +15,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log("new user connected");
 
-    socket.emit('newEmail', {       //emitting the custom event created newEmail	//socket.emit emits event to a single connection
-        from:"varsha@yahoo.in",     // message from server to client				//io.emit emits event to all the connections
-        text:"hello ",
-        createAt: 123
-    });
- 
-//    socket.emit('newMsg',{
-//        from:"sweety",
-//        text:"hii happy holi to you too"
-//    });
-//
-//    socket.on('createEmail',(newEmail) => {     //message from client to server
-//        console.log(newEmail)
-//    });
-
-    socket.on('createMessage',(msg) => {
+	socket.emit('newMsg',{
+		from:"Admin",
+		text:"Welcome to chat app",
+		createdAt:new Date().getTime()
+	});
+	
+	socket.broadcast.emit('newMsg',{
+		from:"Admin",
+		text:"New user joined",
+		createdAt:new Date().getTime()
+	})	
+      socket.on('createMessage',(msg) => {
         console.log(msg)
 		io.emit('newMsg',{
 			from:msg.from,
 			text:msg.text,
 			createdAt:new Date().getTime()
 		})
+		  
+//		socket.broadcast.emit('newMsg',{
+//			from:msg.from,
+//			text:msg.text,
+//			createdAt:new Date().getTime()
+//		})	//sends message to everyone except the person who creates or sends it
     });
 
     socket.on('disconnect',() => {
