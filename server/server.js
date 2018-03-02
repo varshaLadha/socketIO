0482cf9738efca45c33a3ddb3d0b7e9fee2baf31
varsha <lanetteam.varsha@gmail.com>
2018-03-02 11:15:27 +0530

    generateMessage function added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {generateMessage} = require('./utils/message')
+
 const publicPath=path.join(__dirname,"../public");
 const port=process.env.PORT || 3000;
 var app=express();
@@ -15,24 +17,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log("new user connected");
 
-	socket.emit('newMsg',{
-		from:"Admin",
-		text:"Welcome to chat app",
-		createdAt:new Date().getTime()
-	});
+	socket.emit('newMsg',generateMessage('Admin','Welcome to chat app'));
 	
-	socket.broadcast.emit('newMsg',{
-		from:"Admin",
-		text:"New user joined",
-		createdAt:new Date().getTime()
-	})	
+	socket.broadcast.emit('newMsg',generateMessage('Admin','New user joined'))
       socket.on('createMessage',(msg) => {
         console.log(msg)
-		io.emit('newMsg',{
-			from:msg.from,
-			text:msg.text,
-			createdAt:new Date().getTime()
-		})
+		io.emit('newMsg',generateMessage(msg.from,msg.text))
 		  
 //		socket.broadcast.emit('newMsg',{
 //			from:msg.from,
