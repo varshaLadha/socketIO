1fa7d0e5993ccd5c02801546c4f9cc75a6174e80
varsha <lanetteam.varsha@gmail.com>
2018-03-02 08:31:28 +0530

    emiting event to all connections

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,22 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log("connected to server")
+//
+//    socket.emit('createEmail',{
+//        to:"sachin@gmail.com",
+//        text:"email from varsha"
+//    });
 
-    socket.emit('createEmail',{
-        to:"sachin@gmail.com",
-        text:"email from varsha"
-    });
-
-    socket.emit('createMessage',{
-        from:"varsha ladha",
-        message:"hello happy holi"
-    })
+ 
 })
 
 socket.on('disconnect', function(){
     console.log("server disconnected.")
 });
 
-socket.on('newEmail', function (email) {        //creating custom event newEmail
-   console.log("new email",email);
-});
+//socket.on('newEmail', function (email) {        //creating custom event newEmail
+//   console.log("new email",email);
+//});
 
 socket.on('newMsg',function (msg) {
     console.log(msg)
--- server/server.js
+++ server/server.js
@@ -1,44 +1,49 @@
-const path = require('path')
-const express = require('express')
-const socketIO = require('socket.io')
-const http = require('http')
+const path = require('path');
+const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
-const publicPath=path.join(__dirname,"../public")
+const publicPath=path.join(__dirname,"../public");
 const port=process.env.PORT || 3000;
 var app=express();
 
-var server = http.createServer(app)
-var io = socketIO(server)
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
     console.log("new user connected");
 
-    socket.emit('newEmail', {       //emitting the custom event created newEmail
-        from:"varsha@yahoo.in",     // message from server to client
+    socket.emit('newEmail', {       //emitting the custom event created newEmail	//socket.emit emits event to a single connection
+        from:"varsha@yahoo.in",     // message from server to client				//io.emit emits event to all the connections
         text:"hello ",
         createAt: 123
     });
-
-    socket.emit('newMsg',{
-        from:"sweety",
-        text:"hii happy holi to you too"
-    })
-
-    socket.on('createEmail',(newEmail) => {     //message from client to server
-        console.log(newEmail)
-    })
+ 
+//    socket.emit('newMsg',{
+//        from:"sweety",
+//        text:"hii happy holi to you too"
+//    });
+//
+//    socket.on('createEmail',(newEmail) => {     //message from client to server
+//        console.log(newEmail)
+//    });
 
     socket.on('createMessage',(msg) => {
         console.log(msg)
-    })
+		io.emit('newMsg',{
+			from:msg.from,
+			text:msg.text,
+			createdAt:new Date().getTime()
+		})
+    });
 
     socket.on('disconnect',() => {
         console.log("user disconnected")
-    })
-})
+    });
+});
 
 server.listen(port,()=>{
     console.log("Connected to server on "+port)
-})
+});
